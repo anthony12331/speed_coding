be337a6f889aedf9dc53ebbdd67bf40b743b4735
Anthony <markanthonydigaljr@gmail.com>
2026-09-19 12:29:54 +0800

    fixing the bugs

--- public/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('open-btn').addEventListener('click', () => {
-  playFanfare();
-  document.getElementById('landing-screen').classList.add('fade-out');
-  setTimeout(() => {
-    document.getElementById('main-portal').classList.remove('hidden');
-  }, 600);
-});
